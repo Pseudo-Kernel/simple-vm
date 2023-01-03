7eb476a4709935a3adda219c150bb4ed0ac2dd3b
Pseudo-Kernel <22524966+Pseudo-Kernel@users.noreply.github.com>
2023-01-03 18:59:06 +0900

    added missing headers

---
+++ SimpleVM/CoreUnitTest/pop_warnings.h
@@ -0,0 +1,5 @@
+
+#ifdef C_PUSH_WARNING_IGNORE
+#pragma warning(pop)
+#undef C_PUSH_WARNING_IGNORE
+#endif
---
+++ SimpleVM/CoreUnitTest/push_warnings.h
@@ -0,0 +1,5 @@
+
+#ifdef C_PUSH_WARNING_IGNORE
+#pragma warning(push)
+#pragma warning(disable:C_PUSH_WARNING_IGNORE)
+#endif
